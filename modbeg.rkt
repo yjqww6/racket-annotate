c706b4601467b118a634f7e605774da41543a7dc
yjqww6 <343519265@qq.com>
2019-09-02 17:40:58 +0800

    listof ml

--- modbeg.rkt
+++ modbeg.rkt
@@ -1,8 +1,10 @@
 #lang racket/base
 (require (prefix-in p: (only-in racket/base #%module-begin))
-         (for-syntax racket/base syntax/stx "fully-expanded-program.rkt"))
+         syntax/parse/define
+         (for-syntax racket/base
+                     syntax/stx "fully-expanded-program.rkt"))
 
-(define-syntax-rule (define/provide-module-begin pass)
+(define-simple-macro (define/provide-module-begin* pass:id)
   (begin
     (define-syntax (my-module-begin stx)
       (syntax-case stx ()
@@ -15,7 +17,7 @@
                [(_ form (... ...))
                 (stx-map (λ (s) (syntax->FullyExpandedProgram s 'module-level-form))
                          #'(form (... ...)))]))
-           (define transed (map pass prog))
+           (define transed (pass prog))
            (define ret
              #`(#%plain-module-begin
                 #,@(map (λ (s) (FullyExpandedProgram->syntax s 'module-level-form))
@@ -23,4 +25,11 @@
            ret)]))
     (provide (rename-out [my-module-begin #%module-begin]))))
 
-(provide define/provide-module-begin)
+(define-simple-macro (define/provide-module-begin pass:id)
+  (begin
+    (define-for-syntax (f s)
+      (for/list ([s (in-list s)])
+        (pass s)))
+    (define/provide-module-begin* f)))
+
+(provide define/provide-module-begin* define/provide-module-begin)
