ee240424638f9dfa7599f97f1934104d4e737c67
Syntacticlosure <1227847322@qq.com>
2019-09-03 16:15:41 +0800

    add demo-unused-variable-warner

---
+++ examples/demo-unused-variable-warner.rkt
@@ -0,0 +1,110 @@
+#lang racket
+(require "../main.rkt"
+         (for-syntax nanopass/base
+                     syntax/name
+                     syntax/id-set)
+         racket/base)
+
+(provide (except-out (all-from-out racket/base) #%module-begin))
+
+(begin-for-syntax
+  (define (demo prog)
+    (define defined-variables (make-hasheqv))
+    (define used-variables (make-hasheqv))
+    
+    (define (mark-defined! idt phase)
+      (unless (hash-ref defined-variables phase #f)
+        (hash-set! defined-variables phase (mutable-free-id-set #:phase phase)))
+      (free-id-set-add! (hash-ref defined-variables phase)
+                        idt)
+      )
+    (define (mark-used! idt phase)
+      (unless (hash-ref used-variables phase #f)
+        (hash-set! used-variables phase (mutable-free-id-set #:phase phase)))
+      (free-id-set-add! (hash-ref used-variables phase)
+                        idt))
+    
+    (define (collect-ids! ids phase)
+      (syntax-case ids ()
+        [() (void)]
+        [(a . b) (mark-defined! #'a phase)
+                 (collect-ids! #'b phase)]
+        [(a) (mark-defined! #'a phase)]))
+    
+    (define-pass mark-unused-variable : (FullyExpandedProgram ModuleBeginForm) (prog) -> (FullyExpandedProgram ModuleBeginForm) ()
+      (ModuleBeginForm : ModuleBeginForm (prog phase) -> ModuleBeginForm ()
+                       [(#%plain-module-begin ,s ,[ml*] ... ,[ml])
+                        prog])
+      (Expr : Expr (prog phase) -> Expr ()
+            [,x 
+             (mark-used! x phase)
+             prog]
+            [(#%top ,s ,x)
+             (mark-used! x phase)
+             prog]
+            [(#%variable-reference ,s ,x)
+             (mark-used! x phase)
+             prog]
+            [(#%variable-reference-top ,s ,x)
+             (mark-used! x phase)
+             prog]
+            [(#%plain-lambda ,s ,fml ,[body*] ... ,[body])
+             (collect-ids! fml phase)
+             prog]
+            [(case-lambda ,s [,fml ,[body*] ... ,[body]] ...)
+             (for ([f fml])
+               (collect-ids! f phase))
+             prog]
+            [(let-values ,s ([(,x* ...) ,[e]] ...) ,[body*] ... ,[body])
+             (for ([x x*])
+               (collect-ids! x phase))
+             prog]
+            [(letrec-values ,s ([(,x* ...) ,[e]] ...) ,[body*] ... ,[body])
+             (for ([x x*])
+               (collect-ids! x phase))
+             prog]
+            )
+      (GeneralTopLevelForm : GeneralTopLevelForm (prog phase) -> GeneralTopLevelForm ()
+                           [(define-values ,s (,x ...) ,[e])
+                            (collect-ids! x phase)
+                            prog
+                            ]
+                           [(define-syntaxes ,s (,x ...) ,[e0 (+ phase 1) -> e1])
+                            prog]
+                           )
+      (ModuleLevelForm : ModuleLevelForm (prog phase) -> ModuleLevelForm ()
+                       [(begin-for-syntax ,s ,[ml* (+ phase 1) -> ml^] ...)
+                        prog])
+      (SubModuleForm : SubModuleForm (prog phase) -> SubModuleForm ()
+                     )
+      (TopLevelForm : TopLevelForm (prog phase) -> SubModuleForm ()
+                    )
+                        
+      (ModuleBeginForm prog 0)
+      )
+    (mark-unused-variable prog)
+    (for ([(ph vars) defined-variables])
+      (define same-phase-used (hash-ref used-variables ph #f))
+      (when same-phase-used (free-id-set-subtract! vars same-phase-used))
+
+      (for ([(ph1 vars1) used-variables]
+            #:unless (= ph1 ph))
+        (for* ([v vars]
+               [v1 vars1])
+          (when (free-identifier=? v v1 ph ph1)
+            (free-id-set-remove! vars v))))
+      (define unused-sets (filter
+                           (λ (x)
+                             (not (and (eq? '_ (syntax-e x))
+                                       (syntax-original? (syntax-local-introduce x)))))
+                           (free-id-set->list vars)))
+      (unless (null? unused-sets)
+        (error (format "Unused Variables defined at phase ~a : ~a \n"
+                       ph unused-sets)))
+      )
+    
+    prog
+    )
+  )
+
+(define/provide-module-begin* demo)
