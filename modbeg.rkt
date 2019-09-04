0dfa3b96ab438256179b730e8a8f16756df52f3e
yjqww6 <343519265@qq.com>
2019-09-04 14:11:23 +0800

    less macro calls

--- modbeg.rkt
+++ modbeg.rkt
@@ -20,18 +20,21 @@
            ret)]))
     (provide (rename-out [my-module-begin #%module-begin]))))
 
+(begin-for-syntax
+  (define ((module-begin pass) s)
+    (nanopass-case
+     (FullyExpandedProgram ModuleBeginForm) s
+     [(#%plain-module-begin ,s ,ml ...)
+      (with-output-language (FullyExpandedProgram ModuleBeginForm)
+        `(#%plain-module-begin
+          ,s
+          ,(for/list ([ml (in-list ml)])
+             (pass ml))
+          ...))])))
+
 (define-simple-macro (define/provide-module-begin pass:id)
   (begin
-    (define-for-syntax (f s)
-      (nanopass-case
-       (FullyExpandedProgram ModuleBeginForm) s
-       [(#%plain-module-begin ,s ,ml (... ...))
-        (with-output-language (FullyExpandedProgram ModuleBeginForm)
-          `(#%plain-module-begin
-            ,s
-            ,(for/list ([ml (in-list ml)])
-               (pass ml))
-            (... ...)))]))
+    (define-for-syntax f (module-begin pass))
     (define/provide-module-begin* f)))
 
 (provide define/provide-module-begin* define/provide-module-begin)
